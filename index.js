9cf1a59f91032c7cd57da75446eecb41c643d824
mark <mark.kimani3@student.moringaschool.com>
2025-03-13 16:41:39 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()*100) + 1;
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4
+let max = Math.max(1, 2, 3, 20);
+console.log(multiply, random, mod, max);
+
 
 // Write your code here
